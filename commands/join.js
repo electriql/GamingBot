88af8aa34c5f9a5d6aac28e19ab1401edaa78bc0
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2018-11-09 20:16:30 -0800

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -3,12 +3,11 @@ exports.info = "Joins the channel you are currently in."
 
    exports.run = async (message, args, client, ops) => {
         if (message.member.voiceChannel) {
-            if (!message.guild.me.voiceChannel) {
                     message.member.voiceChannel.join()
                        .then(connection => {
                             message.channel.send("**Successfully Joined!**");
                     });
-            }
+            
         }
         else {
             message.channel.send("❌ You must be in a voice channel for me to join!");
