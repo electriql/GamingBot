f69eb24fddac4b710502eb7be3829f02ba75ecad
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2018-11-09 16:56:57 -0800

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -4,14 +4,10 @@ exports.info = "Joins the channel you are currently in."
    exports.run = async (message, args, client, ops) => {
         if (message.member.voiceChannel) {
             if (!message.guild.voiceConnection) {
-                if (message.member.voiceChannel != message.guild.me.voiceChannel) {
-                    message.member.voiceChannel.join().then(connection => {
+                    message.member.voiceChannel.join()
+                       .then(connection => {
                             message.channel.send("**Successfully Joined!**");
                     });
-                }
-                else {
-                    message.channel.send("❌ I am already in your channel!");
-                }
             }
         }
         else {
