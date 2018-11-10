0abad075b80017d46816af3726d05c5fde23e48a
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2018-11-09 20:00:35 -0800

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -4,20 +4,12 @@ let serverData = JSON.parse(fs.readFileSync('Storage/serverData.json', 'utf8'));
 exports.info = "Leaves the channel I am currently in."
     exports.run = async (message, args, client, ops) => {
         let fetched = ops.active.get(message.guild.id);
-        if (message.guild.voiceConnection) {
-            
-            
-            message.guild.voiceConnection.disconnect();
-            message.channel.send("**Successfully Disconnected!**");
-            if (!fetched) {
+        if (!message.guild.me.voiceChannel) return message.channel.send("❌ I am currently not in a voice channel!");
+        message.guild.me.voiceChannel.leave();
 
-            } else {
-                fetched.queue = [];
-                
-                ops.active.set(message.guild.id, fetched);
-            }
-        }
-        else {
-            message.channel.send("❌ I am currently not in a voice channel!");
-        }
+                message.channel.send("**Successfully Disconnected!**");
+
+        if (fetched) fetched.queue = [];
+        ops.active.set(message.guild.id, fetched);
+                    
     }
