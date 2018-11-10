676a19dd013b498b525d0abf4bf404a66c7e68ee
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2018-11-09 20:20:26 -0800

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -3,10 +3,9 @@ exports.info = "Joins the channel you are currently in."
 
    exports.run = async (message, args, client, ops) => {
         if (!message.member.voiceChannel) return message.channel.send("❌ You must be in a voice channel for me to join!");
-                    message.member.voiceChannel.join()
-                       .then(connection => {
-                            message.channel.send("**Successfully Joined!**");
-                    });
+                    let connection = await message.member.voiceChannel.join();
+                    if (connection) return message.channel.send("**Successfully Joined!**");
+                    
             
     }
 
