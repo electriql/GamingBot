b765fa6085f4f590a55c11a53ba38522d116df36
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2019-01-31 18:07:31 -0800

    Add files via upload

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,32 @@
-exports.info = "**Coming Soon!** Makes the bot say anything you would like."
+exports.info = "**Coming Soon!** Makes the bot say anything you would like (Except when profanity is turned on)."
+var profanities = require('profanities');
 
+function filter(message) {
+    var uppr = message.content.toUpperCase();
+    var str = uppr.split(" ");
+
+    for (i = 0; i < str.length; i++) {
+        for (x = 0; x < profanities.length; x++) {
+            if (str[i] == profanities[x].toUpperCase()) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 exports.run = async(message, args, client, ops) => {
-    
+    if (args[0]) {
+        var str = ""
+        for (i = 0; i < args.length; i++) {
+            str = str + args[i] + " ";
+        }
+        if (!filter(message)) {
+            message.channel.send(str)
+            message.delete();
+        }
+
+    }
+    else {
+        message.channel.send("❌ You must give me something to say!")
+    }
 }
