4053ee1ec260db59e540c7468ada3d24e5938eb7
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2019-03-03 18:38:23 -0800

    Update wheel.js

--- commands/wheel.js
+++ commands/wheel.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs');
 let userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
-exports.info = "Spins a built-in 'wheel of fortune' where you can win rewards. " 
+exports.info = "Spins a built-in 'wheel of fortune' where you can win rewards. \n__**Rewards**	__\n💎 = Initial 💎x200 + 💎x100\n💠 = Initial 💎x200 + 💎x50\n🔸 = Initial 💎x200\n🔻 = 💎x150\n🔴 = 💎x100\n⭕ = 💎x50\n❌ = 💎x25\n🚫 = Nothing!"; 
     exports.run = async (message, args, client, ops) => {
         
     }
