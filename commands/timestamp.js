98f0303f99d3178a73c4527d61078b436effd368
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2019-04-15 09:59:12 -0700

    Update timestamp.js

--- commands/timestamp.js
+++ commands/timestamp.js
@@ -1,4 +1,4 @@
-exports.info = "Says when your account was made! Type in another username to find when their account was made!"
+exports.info = "Says when your account was made! Type in another username to find when their account was made! (In Pacific Time)"
 exports.run = async (message, args, client, ops) => {
         var date = message.author.createdAt;
         var member = message.author;
