12830363cffed0188a15853ec0c1effbb40bf27b
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2019-04-14 21:20:08 -0700

    Update timestamp.js

--- commands/timestamp.js
+++ commands/timestamp.js
@@ -1,5 +1,15 @@
-exports.info = "Says when your account was made!"
+exports.info = "Says when your account was made! Type in another username to find when their account was made!"
 exports.run = async (message, args, client, ops) => {
-        let date = message.author.createdAt;
-        message.channel.send("Your account was created on **" + date + "**");
+        var date = message.author.createdAt;
+        var member = message.author;
+        if (args[0]) {
+                if (message.mentions.members.first()) {    
+                        var member = message.mentions.members.first().user;           
+                        var date = member.createdAt;
+                }
+                else {
+                        var member = message.author;
+                }
+        }
+        message.channel.send("**" + member.username + "'s** account was created on **" + date + "**");
 }
