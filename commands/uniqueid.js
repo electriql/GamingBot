c6e64f5798f49090bb5c92f995070a8ad6e489e0
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2019-04-14 20:44:33 -0700

    Delete uniqueid.js

--- commands/uniqueid.js
+++
@@ -1,8 +0,0 @@
-
-
-    exports.info = "Gives your unique user id."
-    exports.run = async (message, args, client, ops) => {
-        message.channel.send('**' + message.author.username + '** Your unique id is '+ message.author.id);
-    }
-
-
