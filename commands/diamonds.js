680cd17c2b590903d7b3b734ab970d59aa70e0a8
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2019-11-17 19:03:39 -0800

    Update diamonds.js

--- commands/diamonds.js
+++ commands/diamonds.js
@@ -22,7 +22,7 @@ exports.run = async (message, args, client, ops) => {
     }
     index.dbSelect(index.pool, 'userdata', 'id', 'diamonds', user.id, function(data) {
         if (!res.rows[0]) {
-                pool.query('INSERT INTO userdata(id, diamonds, daily) VALUES(' + user.id + ',0,0)', [], (err, res) => {
+                index.pool.query('INSERT INTO userdata(id, diamonds, daily) VALUES(' + user.id + ',0,0)', [], (err, res) => {
 
                 })
                 console.log(res.rows);
