9757b61d4b97c9df736c174fed4db4f7e8c727ac
ElectricDiamondW <38442638+ElectricDiamondW@users.noreply.github.com>
2020-08-05 20:00:29 -0700

    Update speak.js

--- commands/speak.js
+++ commands/speak.js
@@ -1,15 +1,21 @@
 exports.category = "fun";
 exports.info = "A command that you aren't supposed to and can't use."
 exports.run = async(message, args, client, ops, serverData) => {
-    if (args[0]) {
-        let str = "";
-        for (i = 0; i < args.length; i++) {
-            str = str + args[i] + " ";
+    if (message.author == ops.owner) {
+        if (args[0]) {
+            let str = "";
+            for (i = 0; i < args.length; i++) {
+                str = str + args[i] + " ";
+            }
+            message.channel.send(str);
         }
-        message.channel.send(str);
+        else {
+            message.channel.send("❌ You must give me something to say!");
+        }
+        message.delete();
     }
     else {
-        message.channel.send("❌ You must give me something to say!");
+        message.channel.send("You shouldn't be using this command...");
     }
-    message.delete();
+    
 }
