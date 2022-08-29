f881bd1c34945000180dc131e57ca26330b8d5aa
Electric <mkchen91021@gmail.com>
2022-08-28 21:31:39 -0700

    fix ssl

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ const conString = process.env.DATABASE_URL;
 
 const pool = new Pool({
     connectionString: conString,
-    ssl: false
-    // ssl: {
-    //      rejectUnauthorized: false
-    // }
+    //ssl: false
+    ssl: {
+         rejectUnauthorized: false
+    }
 })
 var http = require('http');
 
